0d0152b4b6d5cd729a4b1af078fc3918e9455562
moxiegg <aravindh19.7k@gmail.com>
2025-01-12 16:47:25 +0000

    Initial Commit of my dotfiles

---
+++ config/nvim/init.lua
@@ -0,0 +1,9 @@
+require("config.lazy")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("colorscheme kanagawa-wave")
+vim.opt.clipboard = "unnamedplus"
+--install = {colorscheme={"catppuccin"}}
+--vim.cmd.colorscheme "catppuccin"       
